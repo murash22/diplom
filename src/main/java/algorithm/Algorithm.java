d47493cc3863c8af37d9cd2be3daa0e10d39d52c
10023r <ashyr22@icloud.com>
2024-05-13 01:19:12 +0300

    added algorithm class

---
+++ src/main/java/algorithm/Algorithm.java
@@ -0,0 +1,72 @@
+package algorithm;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Algorithm {
+    private SecretKey key;
+    private Cipher ecnCipher;
+    private Cipher decCipher;
+    public final int BLOCK_SIZE = 8;
+
+    public Algorithm(String mode) {
+        try {
+            KeyGenerator generator = KeyGenerator.getInstance("DES");
+            key = generator.generateKey();
+            String cipherInfo = "DES/" + mode + "/NoPadding";
+            ecnCipher = Cipher.getInstance(cipherInfo);  // без паддинга, чтобы дополнять по стандарту 80 00 00
+            ecnCipher.init(Cipher.ENCRYPT_MODE, key);
+            decCipher = Cipher.getInstance(cipherInfo);
+            if (Objects.equals(mode, "CBC")) {
+                decCipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(ecnCipher.getIV()));
+            } else {
+                decCipher.init(Cipher.DECRYPT_MODE, key);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public byte[] encrypt(byte[] plaintext) {
+        byte[] padded = pad(plaintext);
+        try {
+            ecnCipher.init(Cipher.ENCRYPT_MODE, key);
+            return ecnCipher.doFinal(padded);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public byte[] decrypt(byte[] encrypted, boolean removePadding) {
+        try {
+            return removePadding ? unpad(decCipher.doFinal(encrypted)) : decCipher.doFinal(encrypted);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    private byte[] pad(byte[] data) {
+        int padSize = BLOCK_SIZE - data.length % BLOCK_SIZE;
+        byte[] result = new byte[data.length + padSize];
+        System.arraycopy(data, 0, result, 0, data.length);
+        result[data.length] = (byte) 0x80;
+        Arrays.fill(result, data.length + 1, result.length, (byte) 0x0);
+        return result;
+    }
+
+    private byte[] unpad(byte[] data) {
+        int padIndex = data.length - 1;
+
+        while (data[padIndex] == (byte) 0x0) {
+            padIndex -= 1;
+        }
+        return Arrays.copyOfRange(data, 0, padIndex);
+    }
+}
